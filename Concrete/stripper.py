4e7272de5c20371afa61075231a2676113cd9a4f
DVStepter <DVStepter@users.noreply.github.com>
2018-12-04 21:16:55 -0800

    stripper code working

--- Concrete/stripper.py
+++ Concrete/stripper.py
@@ -1,21 +1,22 @@
 import itertools
 import os
+import sys
 
-filter_list = ["PAIR","PHOT","BREM","RAYL","IONI","INEL","CAPT","DECA","ANNI","ESCP","SE","ID","TI","ED","EC","NS","PM","EN","TE","TS"]
+filter_list = ['PAIR','PHOT','BREM','RAYL','IONI','INEL','CAPT','DECA','ANNI','ESCP','SE','ID','TI','ED','EC','NS','PM','EN','TE','TS']
 
-with open("FullConcreteModel.Co60_1332.sim","r") as input: #filters out unwanted interactions
-    with open("New.sim","w") as output:
+with open(sys.argv[1],'r') as input: #filters out unwanted interactions
+    with open('New.sim','w') as output:
         for line in input:
             if all(nb not in line for nb in filter_list):
                 output.write(line)
 
-os.remove("FullConcreteModel.Co60_1332.sim")
+os.remove(sys.argv[1])
 
 lines = [] #Reverses file to be read from bottom to top
-with open('New.sim') as f:
+with open('New.sim', 'r') as f:
     lines = f.readlines()
 with open('New_Rev.sim', 'w') as f:
     for line in reversed(lines):
         f.write(line)
 
-os.remove("New.sim")
+os.remove('New.sim')
